20c38cbbfbe02466efcba6fb6f19170e9a88edff
Charles-Henri Decultot <chdecultot@dokos.io>
2022-08-25 07:49:23 +0200

    First commit

---
+++ lang/en-US.js
@@ -0,0 +1,162 @@
+export default {
+  footer: {
+    terms_and_conditions: 'Terms and conditions',
+    security_and_privacy: 'Security and privacy',
+    jobs: 'Jobs',
+    legal_mentions: 'Legal mentions',
+    become_a_partner: 'Become a partner'
+  },
+  index: {
+    about: {
+      title: 'The software',
+      read_more: 'Read more',
+      cloud: {
+        title: 'Cloud',
+        content: 'Dokos uses the latest web and cloud technologies to guarantee a secure, reliable and performant plateform'
+      },
+      open_source: {
+        title: 'Open-source',
+        content: 'Dokos source code is fully available on Github under the GNU-GPLv3 licence'
+      },
+      project: {
+        title: 'The project',
+        content: 'Dokos is an alternative to proprietary software to which everyone is welcome to participate'
+      }
+    }
+  },
+  cloud: {
+    title: 'The latest web and cloud technologies',
+    contact: 'Contact us',
+    contact_message: 'You have any question regarding the technologies underlying Dokos ?'
+  },
+  open_source: {
+    title: 'Free as in free speech',
+    contact: 'Contact us',
+    contact_message: 'You need any guarantee or support for the usage of the software ?'
+  },
+  project: {
+    title: 'The Dokos project',
+    contact: 'Contact us',
+    contact_message: 'You have any question regarding the project ?'
+  },
+  termsandconditions: {
+    title: 'Terms and conditions'
+  },
+  securityandprivacy: {
+    title: 'Security and privacy'
+  },
+  jobs: {
+    title: 'Job offers',
+    no_offers: 'No current opening',
+    apply: 'Come work with us'
+  },
+  legal: {
+    title: 'Legal mentions'
+  },
+  checkout: {
+    title: 'Welcome to Dokos cloud',
+    step_2: 'Your users',
+    step_3: 'Your billing address',
+    step_4: 'Your payment information',
+    firstname_label: 'Firstname',
+    firstname_placeholder: 'Your firstname',
+    lastname_label: 'Lastname',
+    lastname_placeholder: 'Your lastname',
+    email_label: 'Email',
+    email_placeholder: 'Your email address',
+    company_label: 'Company',
+    company_placeholder: 'Your company\'s name',
+    sitename_label: 'Site URL prefix',
+    sitename_placeholder: 'Your site\'s prefix',
+    password_label: 'Password for the first user',
+    repeat_password_label: 'Repeat your password',
+    password_placeholder: 'Your password',
+    address_line1_label: 'Address',
+    address_line1_placeholder: 'Your address',
+    address_line2_label: 'Additional address details',
+    address_line2_placeholder: 'Your additional address details',
+    postal_code_label: 'Postal code',
+    postal_code_placeholder: 'Your postal code',
+    city_label: 'City',
+    city_placeholder: 'Your city',
+    country_label: 'Country',
+    country_placeholder: 'Your country',
+    users_label: 'Number of users',
+    users_limit: 'For more than 25 users, please contact us',
+    price_per_users_uom: '€ / Month exc.VAT',
+    total_price_uom_with_vat: '€ / Month incl. VAT',
+    total_price_uom: '€ / Month',
+    pending: 'In progress...',
+    submit: 'Start a 14 days trial',
+    subscribe: 'Subscribe',
+    add_users_and_pay: 'Add users and suscribe',
+    pay: 'Pay',
+    error: 'An error prevented the creation of your site.<br>Please contact us.',
+    terms_label: 'I have read and I accept Dokos cloud <a class="cursor-pointer hover:text-dokosBlue-default" href="/en/terms-and-conditions">terms and conditions</a>',
+    previous_step: 'Previous step',
+    last_step: 'Last step',
+    credit_card: 'Credit card',
+    sepa: 'SEPA transfer',
+    reverse_charge: 'Please register your intracommunity VAT identification number to activate the reverse charge mecanism.',
+    vat_number: 'Intracommunity VAT identification number',
+    vat_placeholder: 'Your company VAT number',
+    steps: {
+      step_1: 'Site configuration',
+      step_2: 'N° of Users',
+      step_3: 'Billing address',
+      step_4: 'Payment information'
+    },
+    errors: {
+      firstname_required: 'Firstname is required',
+      lastname_required: 'Lastname is required',
+      email_required: 'Email is required',
+      email_valid: 'Not a valid email address',
+      company_required: 'Company is required',
+      sitename_required: 'Prefix is required',
+      sitename_not_unique: 'Prefix is already used by another company',
+      sitename_not_alphanum: 'Prefix should only contain alphanumeric values',
+      terms_required: 'You need to accept our terms and conditions in order to use Dokos on our cloud',
+      password_required: 'A password is required',
+      password_length: 'Password length must be greater than 6 characters',
+      password_identical: 'Both password fields must match',
+      address_line1_required: 'The address is required',
+      postal_code_required: 'A postal code is required',
+      city_required: 'A city is required'
+    },
+    failed: 'Registration error',
+    failed_message: 'A technical issue has prevented your registration to complete successfully.<br>We will get in touch with you as soon as possible.',
+    gocardless_checkout: 'Please validate your subscription to be redirected to the SEPA payment page.',
+    unavailable: 'Our registration form is currently unavailable. Please try again in a moment.'
+  },
+  checkout_success: {
+    title: 'Thanks for choosing Dokos',
+    success: 'Your site is being created.<br> Please wait a few seconds.',
+    go_to_site: 'Your site is ready at the following address:',
+    error: 'An error prevented the creation of your site.<br>Please contact us.'
+  },
+  error_page: {
+    not_found: 'Sorry, the page you are looking for could not be found',
+    error_occurred: 'Sorry, the website seem to encounter an error',
+    home_page: 'Back to home page'
+  },
+  payment: {
+    confirmation: 'Payment confirmation ...'
+  },
+  feature: {
+    new: 'New'
+  },
+  modules: {
+    selling: 'Sales',
+    purchasing: 'Purchases',
+    inventory: 'Inventory',
+    manufacturing: 'Production',
+    'quality-management': 'Quality Management',
+    accounting: 'Accounting',
+    'human-resources': 'Human Resources',
+    projects: 'Projects',
+    support: 'Support',
+    website: 'Website and Portal',
+    dashboards: 'Dashboards',
+    integrations: 'Integrations'
+  }
+}
---
+++ lang/fr-FR.js
@@ -0,0 +1,163 @@
+export default {
+  footer: {
+    frequently_asked_questions: 'Questions fréquentes',
+    terms_and_conditions: 'Conditions générales',
+    security_and_privacy: 'Sécurité et confidentialité',
+    jobs: 'Recrutements',
+    legal_mentions: 'Mentions légales',
+    become_a_partner: 'Devenez partenaire'
+  },
+  index: {
+    about: {
+      title: 'Le logiciel',
+      read_more: 'En savoir plus',
+      cloud: {
+        title: 'Cloud',
+        content: 'Dokos utilise les dernières technologies web et cloud pour garantir une plateforme sécurisée, fiable et performante'
+      },
+      open_source: {
+        title: 'Open-source',
+        content: 'Le code source de Dokos est entièrement disponible sur Gitlab sous licence GNU-GPLv3'
+      },
+      project: {
+        title: 'Le projet',
+        content: 'Dokos est une alternative aux logiciels propriétaires auquel chacun peut participer'
+      }
+    }
+  },
+  cloud: {
+    title: 'Les dernières technologies web et cloud',
+    contact: 'Contactez nous',
+    contact_message: 'Vous avez des questions concernant les technologies utilisées par Dokos ?'
+  },
+  open_source: {
+    title: 'Un logiciel de gestion sous licence libre',
+    contact: 'Contactez nous',
+    contact_message: 'Vous souhaitez être accompagné ou obtenir des garanties pour l\'utilisation du logiciel ?'
+  },
+  project: {
+    title: 'Le projet Dokos',
+    contact: 'Contactez nous',
+    contact_message: 'Vous avez des questions concernant le projet ?'
+  },
+  termsandconditions: {
+    title: 'Conditions générales'
+  },
+  securityandprivacy: {
+    title: 'Sécurité et confidentialité'
+  },
+  jobs: {
+    title: 'Offres d\'emploi',
+    no_offers: 'Aucune offre actuellement',
+    apply: 'Venez travailler avec nous'
+  },
+  legal: {
+    title: 'Mentions légales'
+  },
+  checkout: {
+    title: 'Bienvenue sur le cloud Dokos',
+    step_2: 'Vos utilisateurs',
+    step_3: 'Votre adresse de facturation',
+    step_4: 'Vos informations de paiement',
+    firstname_label: 'Prénom',
+    firstname_placeholder: 'Votre prénom',
+    lastname_label: 'Nom',
+    lastname_placeholder: 'Votre nom',
+    email_label: 'Email',
+    email_placeholder: 'Votre adresse email',
+    company_label: 'Société',
+    company_placeholder: 'Le nom de votre société',
+    sitename_label: 'Préfixe pour l\'URL de votre site',
+    sitename_placeholder: 'Votre préfixe',
+    users_label: 'Nombre d\'utilisateurs',
+    users_limit: 'Veuillez nous contacter pour plus de 25 utilisateurs',
+    price_per_users_uom: '€ HT / Mois',
+    total_price_uom_with_vat: '€ TTC / Mois',
+    total_price_uom: '€ / Mois',
+    password_label: 'Mot de passe du premier utilisateur',
+    repeat_password_label: 'Répéter le mot de passe',
+    password_placeholder: 'Votre mot de passe',
+    address_line1_label: 'Adresse',
+    address_line1_placeholder: 'Votre adresse',
+    address_line2_label: 'Complément d\'adresse',
+    address_line2_placeholder: 'Votre complément d\'adresse',
+    postal_code_label: 'Code postal',
+    postal_code_placeholder: 'Votre code postal',
+    city_label: 'Ville',
+    city_placeholder: 'Votre ville',
+    country_label: 'Pays',
+    country_placeholder: 'Votre pays',
+    pending: 'En cours...',
+    submit: 'Démarrer un essai de 14 jours',
+    subscribe: 'S\'abonner',
+    add_users_and_pay: 'Ajouter des utilisateurs et s\'abonner',
+    pay: 'Payer',
+    error: 'Une erreur a empêché la création de votre site.<br>Veuillez nous contacter.',
+    terms_label: 'J\'ai lu et j\'accepte les <a class="cursor-pointer hover:text-dokosBlue-default" href="/terms-and-conditions">conditions générales</a> du cloud Dokos',
+    previous_step: 'Étape précédente',
+    last_step: 'Dernière étape',
+    credit_card: 'Carte de crédit',
+    sepa: 'Virement SEPA',
+    reverse_charge: 'Veuillez enregistrer votre numéro de TVA intracommunautaire pour activer le mécanisme d\'autoliquidation.',
+    vat_number: 'Numéro de TVA intracommunautaire',
+    vat_placeholder: 'Le numéro de TVA de votre société',
+    steps: {
+      step_1: 'Configuration du site',
+      step_2: 'N° d\'utilisateurs',
+      step_3: 'Adresse de facturation',
+      step_4: 'Paiement'
+    },
+    errors: {
+      firstname_required: 'Le prénom est obligatoire',
+      lastname_required: 'Le nom est obligatoire',
+      email_required: 'L\'email est obligatoire',
+      email_valid: 'L\'adresse email n\'est pas valide',
+      company_required: 'La société est obligatoire',
+      sitename_required: 'Le préfixe est obligatoire',
+      sitename_not_unique: 'Le préfixe est déjà utilisé par une autre société',
+      sitename_not_alphanum: 'Le préfixe ne peut contenir que des valeurs alphanumériques',
+      terms_required: 'Vous devez accepter nos conditions générales pour utiliser Dokos sur notre cloud',
+      password_required: 'Le mot de passe est obligatoire',
+      password_length: 'Le mot de passe doit être composé de plus de 6 caractères',
+      password_identical: 'Les deux mots de passe ne sont pas identiques',
+      address_line1_required: 'L\'adresse est obligatoire',
+      postal_code_required: 'Le code postal est obligatoire',
+      city_required: 'La ville est obligatoire'
+    },
+    failed: 'Erreur lors de l\'inscription',
+    failed_message: 'Un problème technique a empêché votre inscription d\'aboutir correctement.<br>Nous allons vous contacter dans les plus brefs délais',
+    gocardless_checkout: 'Veuillez valider votre abonnement pour être redirigé vers la page de paiement par prélèvement SEPA.',
+    unavailable: 'Notre formulaire d\'inscription est momentanément indisponible. Veuillez réessayer dans quelques instants.'
+  },
+  checkout_success: {
+    title: 'Merci d\'avoir choisi Dokos',
+    success: 'Votre site est en cours de création.<br> Veuillez patienter quelques instants.',
+    go_to_site: 'Votre site est prêt à l\'adresse suivante:',
+    error: 'Une erreur a empêché la création de votre site.<br>Veuillez nous contacter.'
+  },
+  payment: {
+    confirmation: 'Confirmation de paiement ...'
+  },
+  error_page: {
+    not_found: 'Désolé, la page que vous cherchez est introuvable',
+    error_occurred: 'Veuillez nous excuser, le site semble rencontrer une erreur',
+    home_page: 'Revenir à la page d\'accueil'
+  },
+  feature: {
+    new: 'Nouveau'
+  },
+  modules: {
+    selling: 'Ventes',
+    purchasing: 'Achats',
+    inventory: 'Stocks',
+    manufacturing: 'Production',
+    'quality-management': 'Gestion de la qualité',
+    accounting: 'Comptabilité',
+    'human-resources': 'Ressources Humaines',
+    projects: 'Projets',
+    support: 'Support',
+    website: 'Site web et portail',
+    dashboards: 'Tableaux de bord',
+    integrations: 'Intégrations'
+  }
+}
